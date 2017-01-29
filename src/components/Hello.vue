5511f5a6179d7347bbff5523e799aaec889cbc04
Max McKenzie <hello@maxmckenzie.uk>
2017-01-29 13:48:49 +0100

    dependency added to circle ci, firebase ref adde

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -17,6 +17,7 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <li><a href="https://firebase.google.com/docs/reference/js/" target="_blank">firebase</a></li>
     </ul>
   </div>
 </template>
